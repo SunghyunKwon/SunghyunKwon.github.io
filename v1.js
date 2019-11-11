2431e33ea8a45805cd5b534ec3569f45945de337
Sunghyun Kwon <hobanbird@gmail.com>
2019-11-12 08:45:19 +0900

    Create v1.js

---
+++ v1.js
@@ -0,0 +1,44 @@
+function onButtonClick()
+{
+    navigator.bluetooth.requestLEScan({
+        keepRepeatedDevices: true,
+        acceptAllAdvertisements: true
+    })
+    .then(() => {
+        navigator.bluetooth.addEventListener('advertisementreceived', event => {                        
+            //console.log(event);
+            $('#ready').hide();
+            $('#scanning').show();
+            let appData = event.manufacturerData.get(0x004C);                        
+            console.log(appData);
+            if (appData === undefined) {
+                return;
+            }
+
+            if (appData.byteLength < 23 || appData.byteLength > 31) {
+                return;
+            }
+
+            let major = appData.getUint16(18, false);
+            let minor = appData.getUint16(20, false);
+            let txPowerAt1m = -appData.getInt8(22);
+            let pathLossVs1m = txPowerAt1m - event.rssi;
+            console.log('major: ' + major);
+            console.log('minor: ' + minor);
+            console.log('txPower: ' + txPowerAt1m);
+            console.log('pathLoss: ' + pathLossVs1m);
+            document.getElementById('major').value = major;
+            document.getElementById('minor').value = minor;
+
+        })
+    });
+}
+
+$(document).ready(function () {
+    $('#scanning-modal').modal('show');
+
+    $('#start-scanning').click(function (event) {
+        console.log(event);
+        onButtonClick();
+    })
+})
